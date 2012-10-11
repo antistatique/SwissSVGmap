6a74be512681c4d7828579e96feda16416da56b9
gagarine <perdrisat@gmail.com>
2012-10-11 13:30:32 +0200

    move demo js

--- js/map.js
+++ js/map.js
@@ -137,16 +137,18 @@ SwissMap.prototype.svgLoaded = function(e, svgInstance){
   this.bindEventToSVG();
 };
 
+
 SwissMap.prototype.setSVGAttribut = function(element,attributName, value){
   // Save the old value
   // JS do not help us here...
   if (typeof this.originalSvgData[element.id] === "undefined") {
     this.originalSvgData[element.id] = {};
   }
+  // Only save original value if it was not already done (Def of original right?)
   if (typeof this.originalSvgData[element.id][attributName] === "undefined") {
-    this.originalSvgData[element.id][attributName] = {};
+    this.originalSvgData[element.id][attributName] =  element.getAttribute(attributName);
   }
-  this.originalSvgData[element.id][attributName] =  element.getAttribute(attributName);
+
   //set the new value
   element.setAttribute(attributName, value);
 };
@@ -250,100 +252,7 @@ SwissMap.prototype.onMouseOut = function(e){
   }
 };
 
-//start after svgweb is ready
-window.onsvgload = function(){
-  makeMyMap();
-};
 
-
-function makeMyMap(){
-
-  var mapData = {
-    "swiss": { "name" : "Suisse", "type": "country","file":"suisse.svg" },
-
-    // Cantons
-    "VS": { "name" : "Valais", "type": "canton","file":"suisse.svg", "children_file": 'district_VS.svg'},
-    "GE": { "name" : "Geneva", "type": "canton", "file":"suisse.svg", "children_file": 'district_GE.svg'},
-    "BE": { "name" : "Bern", "type": "canton", "file":"suisse.svg", "children_file": 'district_BE.svg'},
-    "VD": { "name" : "Vaud", "type": "canton", "file":"suisse.svg", "children_file": 'district_VD.svg'},
-    "FR": { "name" : "Fribourg", "type": "canton", "file":"suisse.svg", "children_file": 'district_FR.svg'},
-    "JU": { "name" : "Jura", "type": "canton", "file":"suisse.svg", "children_file": 'district_JU.svg'},
-    "TI": { "name" : "Tessin", "type": "canton", "file":"suisse.svg", "children_file": 'district_TI.svg'},
-    "NE": { "name" : "Neuchâtel", "type": "canton", "file":"suisse.svg", "children_file": 'district_NE.svg'},
-    "SA": { "name" : "Suisse Alémanique", "type": "canton", "file":"suisse.svg", "children_file": 'district_SA.svg'},
-
-    // District VS
-    "ofsnbr2306" : { "name" : "Bezirk Leuk ", "file": 'district_VS.svg', 'parent' : 'VS'},
-    "ofsnbr2309" : { "name" : "Bezirk Raron", "file": 'district_VS.svg', 'parent' : 'VS'},
-    "ofsnbr2303" : { "name" : "District d'Entremont", "file": 'district_VS.svg', 'parent' : 'VS'},
-    "ofsnbr2301" : { "name" : "Bezirk Brig", "file": 'district_VS.svg', 'parent' : 'VS'},
-    "ofsnbr2305" : { "name" : "District d'Hérens", "file": 'district_VS.svg', 'parent' : 'VS'},
-    "ofsnbr2304" : { "name" : "Bezirk Goms", "file": 'district_VS.svg', 'parent' : 'VS'},
-    "ofsnbr2310" : { "name" : "District de Saint-Maurice", "file": 'district_VS.svg', 'parent' : 'VS'},
-    "ofsnbr2307" : { "name" : "District de Martigny", "file": 'district_VS.svg', 'parent' : 'VS'},
-    "ofsnbr2308" : { "name" : "District de Monthey", "file": 'district_VS.svg', 'parent' : 'VS'},
-    "ofsnbr2312" : { "name" : "District  de Sion", "file": 'district_VS.svg', 'parent' : 'VS'},
-    "ofsnbr2302" : { "name" : "District de Conthey", "file": 'district_VS.svg', 'parent' : 'VS'},
-    "ofsnbr2313" : { "name" : "Bezirk Visp", "file": 'district_VS.svg', 'parent' : 'VS'},
-    "ofsnbr2311" : { "name" : "District de Sierre", "file": 'district_VS.svg', 'parent' : 'VS'},
-
-    // District GE
-    "ofsnbr2500" : { "name" : "District de Genève", "file": 'district_GE.svg', 'parent' : 'GE'},
-
-    // District VD
-    "ofsnbr2222" : { "name" : "District de la Broye-Vully", "file": 'district_VD.svg', 'parent' : 'VD'},
-    "ofsnbr2223" : { "name" : "District du Gros-de-Vaud", "file": 'district_VD.svg', 'parent' : 'VD'},
-    "ofsnbr2227" : { "name" : "District de Morges", "file": 'district_VD.svg', 'parent' : 'VD'},
-    "ofsnbr2228" : { "name" : "District de Nyon", "file": 'district_VD.svg', 'parent' : 'VD'},
-    "ofsnbr2229" : { "name" : "District de l'Ouest lausannois", "file": 'district_VD.svg', 'parent' : 'VD'},
-    "ofsnbr2224" : { "name" : "District du Jura-Nord vaudois", "file": 'district_VD.svg', 'parent' : 'VD'},
-    "ofsnbr2225" : { "name" : "District de Lausanne", "file": 'district_VD.svg', 'parent' : 'VD'},
-    "ofsnbr2230" : { "name" : "District de la Riviera-Pays-d'Enhaut", "file": 'district_VD.svg', 'parent' : 'VD'},
-    "ofsnbr2221" : { "name" : "District d'Aigle", "file": 'district_VD.svg', 'parent' : 'VD'},
-    "ofsnbr2226" : { "name" : "District de Lavaux-Oron", "file": 'district_VD.svg', 'parent' : 'VD'},
-
-    // District FR
-    "ofsnbr1001" : { "name" : "District de la Broye", "file": 'district_FR.svg', 'parent' : 'FR'},
-    "ofsnbr1002" : { "name" : "District de la Glâne", "file": 'district_FR.svg', 'parent' : 'FR'},
-    "ofsnbr1007" : { "name" : "District de la Veveyse", "file": 'district_FR.svg', 'parent' : 'FR'},
-    "ofsnbr1003" : { "name" : "District de la Gruyère", "file": 'district_FR.svg', 'parent' : 'FR'},
-    "ofsnbr1004" : { "name" : "District de la Sarine", "file": 'district_FR.svg', 'parent' : 'FR'},
-    "ofsnbr1006" : { "name" : "Bezirk Sense", "file": 'district_FR.svg', 'parent' : 'FR'},
-    "ofsnbr1005" : { "name" : "District du Lac", "file": 'district_FR.svg', 'parent' : 'FR'},
-
-    // Distric JU
-    "ofsnbr2602" : { "name" : "District des Franches-Montagne", "file": 'district_JU.svg', 'parent' : 'JU'},
-    "ofsnbr2603" : { "name" : "District de Porrentruy", "file": 'district_JU.svg', 'parent' : 'JU'},
-    "ofsnbr2601" : { "name" : "District de Delémont", "file": 'district_JU.svg', 'parent' : 'JU'},
-
-    // Distric NE
-    "ofsnbr2404" : { "name" : "District de Neuchâtel", "file": 'district_NE.svg', 'parent' : 'NE'},
-    "ofsnbr2405" : { "name" : "District du Val-de-Ruz", "file": 'district_NE.svg', 'parent' : 'NE'},
-    "ofsnbr2401" : { "name" : "District de Boudry", "file": 'district_NE.svg', 'parent' : 'NE'},
-    "ofsnbr2406" : { "name" : "District du Val-de-Travers", "file": 'district_NE.svg', 'parent' : 'NE'},
-    "ofsnbr2403" : { "name" : "District du Locle", "file": 'district_NE.svg', 'parent' : 'NE'},
-    "ofsnbr2402" : { "name" : "District de la Chaux-de-Fonds", "file": 'district_NE.svg', 'parent' : 'NE'}
-
-  };
-
-  var options = {
-    "overColor" : "#c1aa5b",
-    "normalColor" : "#4D4D4D",
-    "selectedColor" : "#8DB63F",
-    "selectedCallback" : null,
-    "mapsRootPath" : 'maps',
-    //This is just because IE7 don't understand querySelector
-    'overLabel' : document.getElementById('swissmapMouseOverLabel'),
-    'backButton' : document.getElementById('swissmapBack'),
-    'currentTitle' : document.getElementById('swissmapTitle')
-  };
-
-  var mapWrapper = document.getElementById('swissmap');
-  var myMap = new SwissMap(mapWrapper, mapData, 'VS' ,options);
-
-  myMap.init();
-
-}
 
 
 
